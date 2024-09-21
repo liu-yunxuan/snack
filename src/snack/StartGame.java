1867ede15bc2a24c92f7d5324bfbe47dee4d3d6e
liuyunxuan <2126495632@qq.com>
2024-09-22 02:04:27 +0800

    贪吃蛇游戏开发完成

---
+++ src/snack/StartGame.java
@@ -0,0 +1,18 @@
+package snack;
+
+import javax.swing.*;
+
+public class StartGame {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Java-贪吃蛇小游戏");
+
+        frame.setBounds(10,10,915,820); // 设置窗口的位置和大小
+        frame.setResizable(false);  // 窗口大小不可调整,即固定窗口大小
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 设置关闭事件，游戏可以关闭
+
+        //正常游戏界面的绘制
+        frame.add(new GamePanel());
+
+        frame.setVisible(true); // 展示窗口
+    }
+}
